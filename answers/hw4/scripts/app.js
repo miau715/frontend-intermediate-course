ed15ca535f370f6b8098c9bac6cd84fc97d539ab
Miau Huang <miau715@gmail.com>
2017-05-07 01:18:58 +0800

    homework 4 done

--- answers/hw4/scripts/app.js
+++ answers/hw4/scripts/app.js
@@ -4,10 +4,11 @@
     url: "https://api.twitch.tv/kraken/streams/",
     data: { client_id: "oskt3sz9kwmv66y219diqklrvulipi", game: "League of Legends", limit: 20 }
   }).done(function( data ) {
+    $("body").removeClass("loading");
     var list = data.streams;
     var template = $(".list > li:eq(0)");
     $.each( list, function( key, value ) {
-      $(".list").append(template.clone());
+      $(".list").append(template.clone().hide());
       var thisItem = $(".list > li").eq(key+1);
       thisItem.find("a").attr("href", value.channel.url);
       thisItem.find(".screenshot > img").attr("src", value.preview.medium);
@@ -16,5 +17,6 @@
       thisItem.find("h3").text(value.channel.display_name);
     });
     $(".list > li:eq(0)").remove();
+    $(".list > li").fadeIn(500);
   });
 })();
